7f49576c5b8bdc2f59403cec62b35b2bb620bc93
Christopher Guilliams <chris.guilliams@modea.com>
2019-11-17 16:59:25 -0500

    added firebase config for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBCEzlyrdYiF-XjdOXNFuLJHEy5uIMeHzA",
+    authDomain: "christmas-treevia.firebaseapp.com",
+    databaseURL: "https://christmas-treevia.firebaseio.com",
+    projectId: "christmas-treevia",
+    storageBucket: "christmas-treevia.appspot.com",
+    messagingSenderId: "859889492313",
+    appId: "1:859889492313:web:77d5b04bfe676e3f22ac48"
+  }
 };
