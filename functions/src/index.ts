6dbce13a034a9f094dbc262d55b02a4e905ecc0b
Christopher Guilliams <chris.guilliams@modea.com>
2019-11-17 16:55:18 -0500

    changes to firebase functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,17 +1,20 @@
 /* tslint:disable */
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
+//const cors = require('cors')({origin: true});
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 //
+
+admin.initializeApp(functions.config().firebase);
+
 export const helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase!");
 });
 
-export const playTrivia = functions.https.onRequest((request, response) => {
-    admin.firestore().collection('Trivia Plays')
+export const playTrivia = functions.https.onCall((request, response) => {
+    admin.firestore().collection('triviaPlays')
         .add({
             when: admin.firestore.Timestamp.fromDate(new Date())
         });
-    
 });
