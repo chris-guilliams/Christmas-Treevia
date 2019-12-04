61c59a845ba2f0513dc41425fbe1f276e445ad80
Christopher Wood <christopher.wood@modea.com>
2019-12-04 10:39:21 -0500

    IP changed for lights

--- src/app/speech.service.ts
+++ src/app/speech.service.ts
@@ -19,7 +19,7 @@ var answerStatements = ["the answer is", "is the answer"]
 
 @Injectable()
 export class SpeechService {
-  twinkly = new TwinklyLights('192.168.43.92');
+  twinkly = new TwinklyLights('192.168.1.20');
   words$ = new Subject<{[key: string]: string}>();
   errors$ = new Subject<{[key: string]: any}>();
   listening = false;
