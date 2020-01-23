344e20edc7669bede12a0aa6a7e0ef119622c7d7
Christopher Guilliams <chris.guilliams@modea.com>
2020-01-23 07:47:01 -0500

    initial data binding for frequency tilemap component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit, OnDestroy, OnChanges } from '@angular/core';
 import { SpeechService } from './speech.service';
-import { Subscription } from 'rxjs';
+import { Subscription, Observable, of } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Question } from './domain/question';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { Timestamp } from '@google-cloud/firestore';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -15,15 +16,16 @@ export class AppComponent implements OnInit, OnDestroy, OnChanges {
   spokenWords: Subscription;
   recievedAnswers: string[] = [];
   questions: Question[] = [];
-
   hasLoaded = false;
+  timestamps: Observable<any>;
 
   constructor(public speech: SpeechService, private http: HttpClient, private db: AngularFirestore) {
   }
 
   ngOnInit() {
-    this.db.collection('triviaPlays').valueChanges().subscribe((change) => {
-      console.log('The collection was changed', change);
+    this.db.collection('triviaPlays').valueChanges().subscribe((change: {when: Timestamp}[]) => {
+      // console.log('The collection was changed', change);
+      this.timestamps = of(change);
       if (this.hasLoaded) {
         this.speech.startGame();
       } else {
